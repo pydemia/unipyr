acb8b4f7c3d00c75f56a8352f699e712928e340c
Unknown <pydemia@gmail.com>
2017-05-22 18:23:19 +0900

    re-create the package

--- R/dbConnector.R
+++ R/dbConnector.R
@@ -7,14 +7,56 @@
 #' @examples
 #' mdbQuery()
 
+dbStr <- function(){
+    
+    mariadb <- c("mariadb", "org.mariadb.jdbc.Driver", "jdbc:mariadb://%s:%s", "3306")
+    mysql <- c("mysql", "org.mysql.jdbc.Driver", "jdbc:mysql://%s:%s", "3306")
+    postgresql <- c("postgresql", "org.postgresql.Driver", "jdbc:postgresql://%s:%s","5432")
+    ibmdb2 <- c("ibmdb2", "com.ibm.db2.jcc.DB2Driver", "jdbc:db2://%s:%s", "50000")
+    oracle <- c("oracle", "oracle.jdbc.OracleDriver", "jdbc:oracle:thin:@//%s:%s", "1521")
+    dbStrDF <- t(data.frame(mariadb, mysql, postgresql, ibmdb2, oracle))
+    colnames(dbStrDF) <- c("dbname", "classPath", "url", "port")
+    
+    return(dbStrDF)
+    
+}
 
-mdbQuery <- function(query = NULL, username, password){
+dbQuery <- function(db = c("dbname", "jdbcDriverPath", "url", "port"), query = NULL, username, password){
     
-    require(RJDBC)
+    mariadb <- c("mariadb", "org.mariadb.jdbc.Driver", "jdbc:mariadb://%s:%s", "3306")
+    mysql <- c("mysql", "org.mysql.jdbc.Driver", "jdbc:mysql://%s:%s", "3306")
+    postgresql <- c("postgresql", "org.postgresql.Driver", "jdbc:postgresql://%s:%s","5432")
+    ibmdb2 <- c("ibmdb2", "com.ibm.db2.jcc.DB2Driver", "jdbc:db2://%s:%s", "50000")
+    oracle <- c("oracle", "oracle.jdbc.OracleDriver", "jdbc:oracle:thin:@//%s:%s", "1521")
+    dbStrDF <- t(data.frame(mariadb, mysql, postgresql, ibmdb2, oracle))
+    colnames(dbStrDF) <- c("dbname", "classPath", "url", "port")
+
+
+    dbname <- db[1]
+    jdbcPath <- db[2]
+    dburl <- db[3]
+    dbport <- db[4]
     
-    drv <- RJDBC::JDBC("org.mariadb.jdbc.Driver", "~/jdbcDriver/mariadb-java-client-1.5.6.jar") 
-    conn <- RJDBC::dbConnect(drv, "jdbc:mariadb://windows.pydemia.org:3306", username, password)
+    if (dbname %in% rownames(dbStrDF)){
+        classPath <- dbStrDF[dbname, 'classPath']
+        url <- dbStrDF[dbname, 'url']
+        
+        if (dbport == NULL){
+            dbport <- dbStrDF[dbname, 'port']
+        }
+        
+    } else {
+        print("DB type not prepared")
+        break
+    }
+    connURL <- sprintf(url, dburl, port)
+    
+    drv <- RJDBC::JDBC(classPath, jdbcPath)
+    conn <- RJDBC::dbConnect(drv, connURL, username, password)
     res <- RJDBC::dbGetQuery(conn, query)
-    print(res)
+    
+    RJDBC::dbDisconnect(jdbcConnection)
+    print(head(res))
+    
     return(res)
 }
