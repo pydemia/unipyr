d8daa4b7798b98fc36aec38c576a4d9b70c862ae
Unknown <pydemia@gmail.com>
2017-05-22 17:15:49 +0900

    upload changes

--- R/dbConnector.R
+++ R/dbConnector.R
@@ -7,9 +7,9 @@ mdbQuery <- function(query = NULL, username, password){
     
     require(RJDBC)
     
-    drv <- JDBC("org.mariadb.jdbc.Driver", "~/jdbcDriver/mariadb-java-client-1.5.6.jar") 
-    conn <- dbConnect(drv, "jdbc:mariadb://windows.pydemia.org:3306", username, password)
-    res <- dbGetQuery(conn, query)
+    drv <- RJDBC::JDBC("org.mariadb.jdbc.Driver", "~/jdbcDriver/mariadb-java-client-1.5.6.jar") 
+    conn <- RJDBC::dbConnect(drv, "jdbc:mariadb://windows.pydemia.org:3306", username, password)
+    res <- RJDBC::dbGetQuery(conn, query)
     print(res)
     return(res)
 }
